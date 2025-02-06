54463823b06c9a41187e95fe6f8ff0227230c75c
samuelchoi0522 <jaedolc@gmail.com>
2025-02-05 20:03:27 -0600

    Refactor ESLint configuration and update dependencies for improved compatibility

--- pet-adoption-frontend/eslint.config.mjs
+++ pet-adoption-frontend/eslint.config.mjs
@@ -1,27 +1,12 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-import js from "@eslint/js";
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  {
-    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
-  },
-  js.configs.recommended,
-  ...compat.extends(
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "next/core-web-vitals",
-    "next/typescript",
-    "prettier",
-  ),
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
 ];
-
-export default eslintConfig;
