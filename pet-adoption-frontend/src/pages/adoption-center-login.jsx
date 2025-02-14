dcc097f7f0b5050acfadd71b52d16b06114a8a79
kirbydyson <kdysonsc@gmail.com>
2025-02-14 15:15:58 -0600

    adoption center login

---
+++ pet-adoption-frontend/src/pages/adoption-center-login.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import AdoptionCenterLoginComponent from "@/components/adoption-center/AdoptionCenterLoginComponent";
+
+export default function AdoptionCenterLogin() {
+    return (
+        <>
+            <AdoptionCenterLoginComponent />
+        </>
+    );
+}
