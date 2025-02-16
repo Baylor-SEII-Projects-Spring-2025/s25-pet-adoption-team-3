d538b0e697db3d842acf8127ce283aa68da63624
samuelchoi0522 <jaedolc@gmail.com>
2025-02-16 15:03:38 -0600

    Update application properties and GitHub Actions for production environment

--- pet-adoption-api/src/test/java/petadoption/api/PetAdoptionApplicationTests.java
+++ pet-adoption-api/src/test/java/petadoption/api/PetAdoptionApplicationTests.java
@@ -1,12 +1,10 @@
 package petadoption.api;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("testdb")
+@ActiveProfiles("prod")
 class PetAdoptionApplicationTests {
 
 }
