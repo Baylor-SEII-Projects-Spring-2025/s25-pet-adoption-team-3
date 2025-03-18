005dc219ae95ad54205f375dc454a884e7f7213a
franr22 <117682719+franr22@users.noreply.github.com>
2025-03-17 21:25:16 -0500

    Implemented add-pet API

---
+++ pet-adoption-api/src/main/java/petadoption/api/controllers/PetController.java
@@ -0,0 +1,36 @@
+package petadoption.api.controllers;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import petadoption.api.DTO.PetRequestDTO;
+import petadoption.api.models.User;
+import petadoption.api.services.PetService;
+import jakarta.validation.Valid;
+
+
+
+
+
+@CrossOrigin(origins = { "http://localhost:3000", "https://adopdontshop.duckdns.org", "http://35.226.72.131:3000" })
+@RestController
+@RequestMapping("/api/pet")
+public class PetController {
+    private final PetService petService;
+
+    public PetController(PetService petService) {
+        this.petService = petService;
+    }
+
+    @PostMapping("/add-pet")
+    public ResponseEntity<String> addPet(HttpSession session, @RequestBody @Valid PetRequestDTO petRequestDTO) {
+
+        User user = (User) session.getAttribute("user");
+        if (user == null) return ResponseEntity.status(401).body("No active session.");
+        if (user.getRole() != User.Role.ADOPTION_CENTER) return ResponseEntity.status(403).body("Unauthorized action.");
+
+        petService.addPet(user, petRequestDTO);
+        return ResponseEntity.status(201).body(petRequestDTO.getName() + " was successfully added.");
+    }
+
+}
