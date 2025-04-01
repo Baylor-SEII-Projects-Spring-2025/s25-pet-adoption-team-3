7dcd56719436957d04694359a10fcc7685f618b0
samuelchoi0522 <jaedolc@gmail.com>
2025-04-01 11:56:32 -0500

    Add ProfileNavbar to Swipe component and update styles for improved layout

--- pet-adoption-frontend/src/pages/swipe.jsx
+++ pet-adoption-frontend/src/pages/swipe.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { SwipeComponent } from "@/components/swipe/SwipeComponent";
+import ProfileNavbar from "@/components/profile/ProfileNavbar";
 
 export default function Swipe() {
     return (
         <>
+            <ProfileNavbar />
             <SwipeComponent />
         </>
     );
