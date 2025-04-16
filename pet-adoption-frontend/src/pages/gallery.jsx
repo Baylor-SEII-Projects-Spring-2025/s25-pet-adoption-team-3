1f2d962753d105bbcf6ce8375ef13d7931cd3c7a
kirbydyson <kdysonsc@gmail.com>
2025-04-16 01:09:26 -0500

    add nav bar

--- pet-adoption-frontend/src/pages/gallery.jsx
+++ pet-adoption-frontend/src/pages/gallery.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import GalleryComponent from "@/components/gallery/GalleryComponent";
+import ProfileNavbar from "@/components/profile/ProfileNavbar";
 
 export default function Gallery() {
     return (
         <>
+            <ProfileNavbar />
             <GalleryComponent />
         </>
     );
