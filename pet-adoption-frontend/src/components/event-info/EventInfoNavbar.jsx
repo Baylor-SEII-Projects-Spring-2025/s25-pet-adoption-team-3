8fbef416b0118f2e56f26b389f4692d36c696e24
samuelchoi0522 <jaedolc@gmail.com>
2025-05-05 16:56:29 -0500

    Replace "Locations" link with "Learn" link in multiple navigation components

--- pet-adoption-frontend/src/components/event-info/EventInfoNavbar.jsx
+++ pet-adoption-frontend/src/components/event-info/EventInfoNavbar.jsx
@@ -188,7 +188,7 @@ export default function EventInfoNavbar() {
             </div>
             <ul className={styles.navLinks}>
                 <li>
-                    <Link href="/locations">Locations</Link>
+                    <Link href="/learn">Learn</Link>
                 </li>
                 <li>
                     <Link href="/gallery">Gallery</Link>
